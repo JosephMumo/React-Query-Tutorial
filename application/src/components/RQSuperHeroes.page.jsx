9d9f82637bd5ba1bcc6ae475353b07c5e4919d2c
JosephMumo <josephmumo25@gmail.com>
2023-09-07 17:22:26 +0300

    working with staletime

--- application/src/components/RQSuperHeroes.page.jsx
+++ application/src/components/RQSuperHeroes.page.jsx
@@ -8,7 +8,7 @@ export const RQSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
   }
 
-  const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes, { cacheTime: 5000 })
+  const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes, { staleTime: 30000 })
 
   if (isLoading) {
     return <h2>Loading . . .</h2>
